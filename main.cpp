c0d7e75c721a3b4afb3d5329679bed8945191660
jermsn <jermsn@gmail.com>
2018-04-09 19:36:41 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CS2Project_ContactManager.cpp
+// Author      : Jeremy Norvell
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Program Ending. Have a nice day." << endl; // prints Program Ending. Have a nice day.
+	return 0;
+}
