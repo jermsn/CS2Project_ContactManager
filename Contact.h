030e2b11da3b4e0db834dd194c5077e99b0a9f35
jermsn <jermsn@gmail.com>
2018-04-09 19:49:41 -0700

    first commit

---
+++ Contact.h
@@ -0,0 +1,34 @@
+/*
+ * Contact.h
+ *
+ *  Created on: Apr 9, 2018
+ *      Author: student
+ */
+
+#ifndef CONTACT_H_
+#define CONTACT_H_
+
+using namespace std;
+
+class Contact
+{
+private:
+	char firstName[30]; 	// c-string of first name
+	char lastName[30];		// c-string of last name
+	string birthDate;		// c-string of birth date, formatted mm/dd/yyyy
+	string homePhone;		// c-string of home phone, formated (NNN) NNN-NNNN
+	string mobilePhone;		// c-string of mobile phone, formated (NNN) NNN-NNNN
+	string workPhone;		// c-string of work phone, formated (NNN) NNN-NNNN
+	string addrLine1;		// first line of mailing address
+	string addrLine2;		// second line of mailing address
+	string city;			// city of mailing address
+	string state;			// state of mailing address
+	string zip;				// zip code of mailing address
+public:
+
+};
+
+
+
+
+#endif /* CONTACT_H_ */
