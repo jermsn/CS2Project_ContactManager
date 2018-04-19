ecdf0e47e51049442a8997818648a40b7da1686d
jermsn <jermsn@gmail.com>
2018-04-19 07:29:24 -0700

    Zipcode Input Mask correction

--- Zipcode.cpp
+++ Zipcode.cpp
@@ -45,7 +45,7 @@ istream &operator>>( istream &input, Zipcode &inZip)
 	input.get( tempIn, LEN );
 	if (!( input.gcount() == 10 || input.gcount() == 5 ) ) 		// case must be 99999 or 99999-9999
 	{
-		cout << "Invalid length: " << input.gcount() << endl;
+		//cout << "Invalid length: " << input.gcount() << endl;
 		input.clear( ios::failbit );
 	}	// or set bad bit
 	else 														// proper count of characters - checking format
@@ -54,17 +54,17 @@ istream &operator>>( istream &input, Zipcode &inZip)
 		{
 			if ( (i >= 0 && i <= 4) && !isdigit(tempIn[i]) ) 		// case input must be numeric
 			{
-				cout << "Invalid character " << tempIn[i] << " at position " << i << endl;
+				//cout << "Invalid character " << static_cast<int>(tempIn[i]) << " at position " << i << endl;
 				input.clear( ios::failbit ); 						// set bad bit
 			}
-			if ( i == 5 && !(tempIn[i] == 55 || tempIn[i] == 32 ))	// case 6th char is dash or space
+			if ( i == 5 && !(tempIn[i] == 45 || tempIn[i] == 32 ))	// case 6th char is dash or space
 			{
-				cout << "Invalid character " << tempIn[i] << " at position " << i << endl;
+				//cout << "Invalid character " << static_cast<int>(tempIn[i]) << " at position " << i << endl;
 				input.clear( ios::failbit ); 						// set bad bit
 			}
 			if ( (i >= 6 && i <= 8) && !isdigit(tempIn[i]) )		// case chars 7-10 must be numeric
 			{
-				cout << "Invalid character " << tempIn[i] << " at position " << i << endl;
+				//cout << "Invalid character " << static_cast<int>(tempIn[i]) << " at position " << i << endl;
 				input.clear( ios::failbit ); 						// set bad bit
 			}
 		}
